02acfc48eeaaae66b33a14a28a31b04d25001257
SillyFreak <sillyfreak.ck@gmail.com>
2017-08-24 14:51:27 +0200

    initial version

--- README.rst
+++ README.rst
@@ -1,2 +1,31 @@
 Hedgehog Tester
 ===============
+
+This project contains a small hardware test program for the Hedgehog controller.
+The program continuously moves servos and motors, and shows sensor values on the console.
+
+Usage
+-----
+
+Start the program and connect actuators to see that they are moving back and forth,
+and connect sensors to check their output values.
+
+To test properly, make sure to observe motors moving in both directions,
+as an improperly soldered H-bridge may only affect one direction of movement.
+
+The sensor output consists of 16 dots that change into other ASCII characters depending on sensor values::
+
+    ......=. ........
+    ......=. ........
+    -.....=. ........
+
+    -.....-. ......#.
+    =....... ......#.
+    #....... ........
+    #....... ........
+
+The above sample output shows analog sensors on ports 0 and 6 being adjusted,
+and a push button on port 14 being pressed shortly.
+Button 15 is configured as emergency stop, so make that port the last when testing.
+Observing the empty line scrolling by helps confirming that the tester program is running properly,
+even when there are no changes in sensor values and the console output therefore doesn't change.
---
+++ main.py
@@ -0,0 +1,48 @@
+from time import sleep
+from hedgehog.client import connect
+
+from math import sin, pi
+import sys
+
+target = sys.argv[1] if len(sys.argv) > 1 else 'raspberrypi'
+print("Hello {}".format(target))
+
+with connect('tcp://{}.local:10789'.format(target), emergency=15) as hedgehog:
+    for i in range(16):
+        hedgehog.set_input_state(i, True)
+
+    def motors():
+        while True:
+            for i in range(20):
+                speed = int(1000 * sin(2 * pi * i / 20))
+                for port in range(4):
+                    hedgehog.move(port, speed)
+                yield
+
+    def servos():
+        while True:
+            for i in range(10):
+                pos = int(1024 + 900 * sin(2 * pi * i / 10))
+                for port in range(4):
+                    hedgehog.set_servo(port, True, pos)
+                yield
+
+    def sensors():
+        while True:
+            analogs = [hedgehog.get_analog(port) for port in range(8)]
+            digitals = [hedgehog.get_digital(port) for port in range(8, 16)]
+
+            print("".join('#=-.'[int(a / 1024)] for a in analogs) + " " +
+                  "".join('.' if d else '#' for d in digitals))
+            yield
+
+    m = motors()
+    s = servos()
+    io = sensors()
+
+    for i in range(20000):
+        next(m)
+        next(s)
+        next(io)
+        if i % 20 == 0:
+            print()
