b1eb017d10570587ccd91dd32a195c8b5711cca3
Clemens Koza <koza@pria.at>
2019-09-26 15:03:20 +0200

    update to new APIs, include IMU (only accel) and speaker

--- main.py
+++ main.py
@@ -17,37 +17,52 @@ with connect('tcp://{}.local:10789'.format(target), emergency=15) as hedgehog:
                 for i in range(20):
                     speed = int(1000 * sin(2 * pi * i / 20))
                     for port in range(4):
-                        hedgehog.move(port, speed)
+                        hedgehog.move_motor(port, speed)
                     yield
 
         def servos():
             while True:
                 for i in range(10):
                     pos = int(1024 + 900 * sin(2 * pi * i / 10))
-                    for port in range(4):
-                        hedgehog.set_servo(port, True, pos)
+                    for port in range(6):
+                        hedgehog.set_servo(port, pos)
                     yield
 
         def sensors():
             while True:
-                analogs = [hedgehog.get_analog(port) for port in range(8)]
-                digitals = [hedgehog.get_digital(port) for port in range(8, 16)]
+                analogs = [hedgehog.get_analog(port) for port in range(16)]
+                digitals = [hedgehog.get_digital(port) for port in range(16)]
+                accel = hedgehog.get_imu_acceleration()
 
                 print("".join('#=-.'[int(a / 1024)] for a in analogs) + " " +
-                      "".join('.' if d else '#' for d in digitals))
+                      "".join('.' if d else '#' for d in digitals) + " (" +
+                      ", ".join(f"{val / 8192:5.2f}" for val in accel) + ")")
                 yield
+
+        def speaker():
+            while True:
+                hedgehog.set_speaker(440)
+                for _ in range(1):
+                    yield
+                hedgehog.set_speaker(None)
+                for _ in range(9):
+                    yield
 
         m = motors()
         s = servos()
         io = sensors()
+        sp = speaker()
 
-        for i in range(20000):
+        i = 0
+        while True:
+            i += 1
             next(m)
             next(s)
             next(io)
-            if i % 20 == 0:
+            next(sp)
+            if i % 10 == 0:
                 print()
-            sleep(0.5)
+            sleep(0.01)
 
     run()
 
